f83e4734ddef2888d5bb8c76bde4f3fbef90d63e
Ivan Milin <101069339+ivanMilin@users.noreply.github.com>
2024-09-02 13:49:05 +0200

    Add files via upload

---
+++ scripts/create_project.tcl
@@ -0,0 +1,41 @@
+# ===================================================================================
+# Definisanje direktorijuma u kojem ce biti projekat
+# ===================================================================================
+cd ..
+set root_dir [pwd]
+cd scripts
+set resultDir ../vivado_project
+
+file mkdir $resultDir
+
+create_project RISCV_CPU $resultDir -part xc7z020clg400-1
+set_property board_part digilentinc.com:zybo-z7-20:part0:1.1 [current_project]
+
+# ===================================================================================
+# Ukljucivanje svih izvornih i simulacionih fajlova u projekat
+# ===================================================================================
+add_files -norecurse ../hdl/Add4.sv
+add_files -norecurse ../hdl/ALU.sv
+add_files -norecurse ../hdl/BranchCondition.sv
+add_files -norecurse ../hdl/Controller.sv
+add_files -norecurse ../hdl/DataMemory.sv
+add_files -norecurse ../hdl/ImmediateGenerator.sv
+add_files -norecurse ../hdl/InstructionMemory.sv
+add_files -norecurse ../hdl/Mux2.sv
+add_files -norecurse ../hdl/PC.sv
+add_files -norecurse ../hdl/Processor.sv
+add_files -norecurse ../hdl/RegisterFile.sv
+add_files -norecurse ../hdl/WriteBack.sv
+#add_files -norecurse ../hdl/code.mem
+
+update_compile_order -fileset sources_1
+add_files -norecurse ../hdl/code.mem
+
+update_compile_order -fileset sources_1
+
+set_property SOURCE_SET sources_1 [get_filesets sim_1]
+
+add_files -fileset sim_1 -norecurse ../tb/Processor_TB.sv
+
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
